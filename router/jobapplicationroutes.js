272376b6de10fa1b06fa8b3f4598e83dfc86c593
Rakshita@123 <rakshitajanoji@gmail.com>
2025-08-01 15:18:37 +0530

    fghjkl

--- router/jobapplicationroutes.js
+++ router/jobapplicationroutes.js
@@ -1,9 +1,7 @@
 import express from 'express'
 
 const router=express.Router();
-import {applyjob, getapplicationsoflogineduser,updateapplication} from '../controller/jobapplicationcontroller.js';
-
-
+import {applyjob, getapplicationsoflogineduser,updateapplication,deleteapplication} from '../controller/jobapplicationcontroller.js';
 router.post('/apply/:jobid/:userid',applyjob);
 router.get('/apply/:userid',getapplicationsoflogineduser);
 router.put('/updateapplication/:userid',updateapplication);
